9289b5309119850cff3099675758960af5adcbb6
Gary Fredericks <fredericksgary@gmail.com>
2012-08-09 23:02:11 -0500

    Don't generate empty top-level lets

--- src/currj/core.clj
+++ src/currj/core.clj
@@ -5,11 +5,15 @@
 
 (defmacro fn
   [arg-list body]
-  (let [[lettings form] (ci/refactor (wk/macroexpand-all body) arg-list)]
-    `(let [~@(apply concat lettings)]
-       (clojure.core/fn
-        ~@(for [n (range 1 (count arg-list))
-                :let [[given not-given] (split-at n arg-list)]]
-            `([~@given]
-                (fn [~@not-given] ~form)))
-        ([~@arg-list] ~form)))))
+  (let [[lettings form] (ci/refactor (wk/macroexpand-all body) arg-list)
+        fn-form
+        `(clojure.core/fn
+          ~@(for [n (range 1 (count arg-list))
+                  :let [[given not-given] (split-at n arg-list)]]
+              `([~@given]
+                  (fn [~@not-given] ~form)))
+          ([~@arg-list] ~form))]
+    (if (empty? lettings)
+      fn-form
+      `(let [~@(apply concat lettings)]
+         ~fn-form))))
