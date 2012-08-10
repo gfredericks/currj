54f72495c29d4bdc5762119342324c84618a40a2
Gary Fredericks <garyf@groupon.com>
2012-08-10 16:19:23 -0500

    Use clojure.tools.macro instead of clojure.walk

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject currj "0.0.1"
   :description "Currying in Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.macro "0.1.1"]])
--- src/currj/core.clj
+++ src/currj/core.clj
@@ -1,11 +1,11 @@
 (ns currj.core
   (:refer-clojure :exclude [fn])
-  (:require [clojure.walk :as wk] ; TODO: use the clojure.tools.macro version instead
+  (:require [clojure.tools.macro :as macro]
             [currj.independence :as ci]))
 
 (defmacro fn
   [arg-list body]
-  (let [[lettings form] (ci/refactor (wk/macroexpand-all body) arg-list)
+  (let [[lettings form] (ci/refactor (macro/mexpand-all body) arg-list)
         fn-form
         `(clojure.core/fn
           ~@(for [n (range 1 (count arg-list))
