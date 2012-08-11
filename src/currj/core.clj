6170e57af91453bdb4537d4481ffc17f8a752287
Gary Fredericks <fredericksgary@gmail.com>
2012-08-11 15:03:23 -0500

    Another tweak for slightly more readable generated code

--- src/currj/core.clj
+++ src/currj/core.clj
@@ -6,13 +6,16 @@
 (defmacro fn
   [arg-list body]
   (let [[lettings form] (ci/refactor (macro/mexpand-all body) arg-list)
+        partial-bodies (for [n (range 1 (count arg-list))
+                             :let [[given not-given] (split-at n arg-list)]]
+                         `([~@given]
+                             (fn [~@not-given] ~form)))
         fn-form
-        `(clojure.core/fn
-          ~@(for [n (range 1 (count arg-list))
-                  :let [[given not-given] (split-at n arg-list)]]
-              `([~@given]
-                  (fn [~@not-given] ~form)))
-          ([~@arg-list] ~form))]
+        (if (empty? partial-bodies) ;; for more readable output
+          `(clojure.core/fn [~@arg-list] ~form)
+          `(clojure.core/fn
+            ~@partial-bodies
+            ([~@arg-list] ~form)))]
     (if (empty? lettings)
       fn-form
       `(let [~@(apply concat lettings)]
